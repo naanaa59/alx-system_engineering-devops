97e2a6854e9f9a6b8e135fef7b13c7711acd4d67
oumaima <oumaimanaanaapro@gmail.com>
2024-02-23 22:10:42 +0100

    1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  path    => ['/usr/bin'],
-  unless  => '/usr/bin/pip3 show Flask | grep Version | grep -q 2.1.0',
+#!/usr/bin/pup
+# Install Flask 2.1.0
+package {'flask':
+ensure   => '2.1.0',
+provider => 'pip3'
 }
-
