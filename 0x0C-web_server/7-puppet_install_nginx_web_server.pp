5e844bdfffbe222af0e01a4f271c364da9d796de
oumaima <oumaimanaanaapro@gmail.com>
2024-02-28 00:28:42 +0100

    7

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,51 +1,25 @@
 # This is a puppet manifest that do the same config as previous bash script
 
-class nginx_install {
-	package { 'nginx':
-		ensure => 'installed',
-		}
-	}
-
-class nginx_configure {
-	file { '/var/www/html/index':
-		ensure  => file,
-		content => 'Hello World',
-	}
-
-	file { '/var/www/html/404.html':
-		ensure  => file,
-		content => "Ceci n'est pas une page\n",
-	}
-
-	file { '/etc/nginx/sites-available/default':
-		ensure  => present,
-		content => "server {
-  		  	  	   listen 80 default_server;
-        			   listen [::]:80 default_server;
-               				root /var/www/html;
-        			   index index.html index.htm index.nginx-debian.html;
-        			   server_name _;
-        			   location / {
-                			try_files \$uri \$uri/ =404;
-        			   }
-        			   error_page 404 /404.html;
-        			   location  /404.html {
-            				internal;
-        			  }
-        			  if (\$request_filename ~ redirect_me){
-            				rewrite ^ https://www.youtube.com/ permanent;
-        			  }
+exec { 'apt-get-update':
+	command => '/usr/bin/apt-get update',
 }
-",
-		notify  => Service['nginx'],
+package { 'nginx':
+	ensure  => installed,
+	require => Exec['apt-get-update'],
 	}
 
-	service { 'nginx':
-		ensure    => running,
-		enable    => true,
-		subscribe => [File['/etc/nginx/sites-available/default']],
-	}
+
+file { '/var/www/html/index.html':
+	content => 'Hello World',
+	require => Package['nginx'],
 }
-class { 'nginx_install': }
-class { 'nginx_configure':}
+
+exec { 'redirect_me':
+	command => 'sed -i "/listen 80 default_server;/a rewrite ^/redirect_me https://www.youtube.com/ permanent;" /etc/nginx/sites-available/default',
+	provider => 'shell'
+}
+service { 'nginx':
+	ensure  => running,
+	require => Package['nginx'],
+}
 
