0f6650cec77f72c2d5abe0833a70aedec9da63c4
oumaima <oumaimanaanaapro@gmail.com>
2024-03-05 00:56:32 +0100

    2 last

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,4 +1,4 @@
-# Use Puppet to automate the task of creating a custom HTTP header response
+# automate the task of creating a custom HTTP header response, but with Puppet.
 
 exec {'update':
   command => '/usr/bin/apt-get update',
