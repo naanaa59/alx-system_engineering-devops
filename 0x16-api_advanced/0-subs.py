c93a35a238b0113c7b5bc823fae6a7314ce9ca7a
oumaima <oumaimanaanaapro@gmail.com>
2024-04-09 16:55:36 +0000

    task0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+""" This script return the numbers of subscribers to Reddit API """
+
+import requests
+import sys
+
+
+def number_of_subscribers(subreddit):
+    url = 'https://www.reddit.com/r/'
+    headers = {'User-Agent': 'Oumaima'}
+
+    response = requests.get(f'{url}{subreddit}/about.json',
+                            headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
+        return 0
